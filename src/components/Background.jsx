28cc410b3a2c257f111d77c409d9ca7256e0d98f
YanisOuld <yanis.ould-mahammed@polymtl.ca>
2026-03-14 21:45:45 -0400

    feat: new bg slowness

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,14 +6,8 @@ export default function Background() {
       <div className="orb orb-1" />
       <div className="orb orb-2" />
       <div className="ripple rp-1" />
-      <div className="ripple rp-1b" />
       <div className="ripple rp-2" />
-      <div className="ripple rp-2b" />
-      <div className="ripple rp-3" />
-      <div className="ripple rp-3b" />
-      <div className="ripple rp-4" />
       <div className="ripple rp-5" />
-      <div className="ripple rp-6" />
     </div>
   );
 }
